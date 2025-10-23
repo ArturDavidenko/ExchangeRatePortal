8c94ca46306dd92ae35b3f2f295bea05588f6da8
ArtursDavidenko <artursdavidenko@gmail.com>
2025-10-23 16:14:16 +0300

    Init commit

---
+++ ExchangeRatesAPI/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRatesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExchangeRatesController : ControllerBase
+    {
+        
+    }
+}
